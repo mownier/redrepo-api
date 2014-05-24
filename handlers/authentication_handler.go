0b5fb9317e00e08964525b599dc18c7b91cbb65a
Mounir Ybanez <rinuom91@gmail.com>
2014-05-24 18:53:59 +0800

    Set up the handlers. As of now, the error_handler and authentication_handler

---
+++ handlers/authentication_handler.go
@@ -0,0 +1,9 @@
+// authentication_handler.go
+// @author Mounir Ybanez
+// @date May 24, 2014
+
+package handlers
+
+type AuthenticationHandler struct {
+
+}
---
+++ handlers/error_handler.go
@@ -0,0 +1,9 @@
+// error_handler.go
+// @author Mounir Ybanez
+// @date May 24, 2014
+
+package handlers
+
+type ErrorHandler struct {
+
+}
