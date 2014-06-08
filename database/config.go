bbcaa6bc932cc6a89e916c30511697b088d57ad7
Mounir Ybanez <rinuom91@gmail.com>
2014-06-08 17:54:54 +0800

    Added database configuration

---
+++ database/config.go
@@ -0,0 +1,34 @@
+// config.go
+// @author Mounir Ybanez
+// @date June 8, 2014
+
+package database
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+    "github.com/coopernurse/gorp"
+)
+
+func OpenDatabase() *gorp.DbMap {
+	// connect to db using standard Go database/sql API
+    // use whatever database/sql driver you wish
+    db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/redrepo")
+	if err != nil {
+		panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
+	}
+
+	// Open doesn't open a connection. Validate DSN data:
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+
+	// construct a gorp DbMap
+	dbmap := &gorp.DbMap{Db: db, Dialect: gorp.MySQLDialect{"InnoDB", "UTF8"}}
+	return dbmap
+}
+
+func CloseDatabase(dbmap *gorp.DbMap) {
+	dbmap.Db.Close()
+}
---
+++ database/entries.go
@@ -0,0 +1,22 @@
+// entries.go
+// @author Mounir Ybanez
+// @date June 8, 2014
+
+package database
+
+type BaseEntry struct {
+	Id int64 `db:"id"`
+}
+
+type Account struct {
+	BaseEntry
+	FirstName 	string 	`db:"first_name"`
+	LastName 	string 	`db:"last_name"`
+	Email 		string 	`db:"email"`
+	Password	string
+	Username 	string 	`db:"username"`
+	BloodType 	string 	`db:"blood_type"`
+	Latitude 	float64	`db:"latitude"`
+	Longitude 	float64	`db:"longitude"`
+	DateJoined	string	`db:"date_joined"`
+}
